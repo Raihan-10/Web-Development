68eeb714bd50243870fb1004dcf7c7c625861ebc
Raihan-10 <rsraihan1233@gmail.com>
2026-05-15 20:16:15 +0600

    Done full project with meaningful README file

--- Job Tracker/script.js
+++
@@ -1,6 +0,0 @@
-const allBtn =  document.getElementById('all-btn');
-const interviewBtn = document.getElementById('interview-btn');
-const rejectedBtn = document.getElementById('rejected-btn');
-
-// toggle
-const tabs = [allBtn, interviewBtn, rejectedBtn];
